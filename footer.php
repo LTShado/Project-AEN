f07cbce081032d6234b1be34249d0aac84bc5327
LTShado <56226077+LTShado@users.noreply.github.com>
2020-06-25 21:56:23 +0200

    Add files via upload
    first upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+		<footer>
+			<div class="nous">
+				<div class="col-3 contact">
+					<span>Nous contacter</span>
+				</div>
+				<div class="col-3 follow">
+					<span>Nous suivre</span>
+				</div>
+			</div>
+			<br><br>
+			<span class="copyright" >©BAZOIN Dorian</span>
+		</footer>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php session_start(); ?>
+<!Doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> Accueil </title>
+    <link rel="stylesheet" href="CSS/CSS.css">
+    <link rel="stylesheet" href="CSS/bootstrap.css">
+</head>
+
+<body>
+  <?php
+      include("header.php");
+  ?>
+  <main>
+    <div>
+        <div class="img_back">
+            <!--<img id="img_back" src="images/background.png">-->
+        </div>
+        <div>
+          <section>
+            <h1>Présentation</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </section>
+        </div>
+        </div>
+    </div>
+    </main>
+    <?php
+        include("footer.php");
+    ?>
+    </body>
+</html>
